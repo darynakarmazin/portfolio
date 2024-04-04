bdf669972d5b3598b057579a37ce171caef2e100
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-04-04 17:14:47 +0300

    Updated data.json with new projects

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
 # PORTFOLIO.
 
 Daryna Karmazin
-Junior Frontend Developer
+Junior Frontend React Developer
 
-I am a highly motivated and independent candidate seeking an entry-level position. I havetheoretical knowledge and practical skills in working with test and real projects. I have beencoding for 1 year and during this time I have done 3 successful team projects and 4 individualones. Furthermore, I quickly adapt to new conditions and responsibilities.
+I am a highly motivated and independent candidate looking for an entry-level position. With a year of coding experience, I possess both theoretical knowledge and practical skills gained from involvement in multiple successful team and individual projects. My main stack is React and Node.js. Furthermore, I quickly adapt to new conditions and responsibilities.
 
 # Hard Skills
+
 HTML5 / CSS3
-JavaScript
+JavaScript / TypeScript
 React / React hooks
 React Native
 Redux / Redux Toolkit
 REST API
-Node.js / MongoDB
-Git / Gitlab / Github
+Node.js / Express.js
+MongoDB / Mongoose
+Git / Github
 Trello scrum boards
 Test cases Test Rail / TestLink
 Mantis Bug Tracker / JIRA Software
 
 # Soft Skills
+
 Attention to detail
 Teamwork
 Organized
 Good communication
 
 # Contacts
+
 +38 063 468 41 07
 daryna.karmazin@gmail.com
---
+++ js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const portfolioList = document.getElementById("portfolio-list");
+
+      data.portfolio.forEach((project) => {
+        const card = document.createElement("li");
+        card.classList.add("card");
+
+        const link = document.createElement("a");
+        link.classList.add("card-link");
+        link.target = "_blank";
+        link.href = project.link;
+
+        const img = document.createElement("img");
+        img.classList.add("card-img");
+        img.src = project.imgSrc;
+        img.alt = project.title;
+
+        const title = document.createElement("h3");
+        title.classList.add("card-title");
+        title.textContent = project.title;
+
+        const tools = document.createElement("p");
+        tools.classList.add("card-tools");
+        tools.textContent = project.tools;
+
+        const description = document.createElement("p");
+        description.classList.add("card-description");
+        description.textContent = project.description;
+
+        link.appendChild(img);
+        link.appendChild(title);
+        link.appendChild(tools);
+        link.appendChild(description);
+
+        card.appendChild(link);
+        portfolioList.appendChild(card);
+      });
+    })
+    .catch((error) => console.error("Error loading portfolio data:", error));
+});
